c314fd7bf95cdb086de0ab22fece59db76b76cdb
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2020-05-27 14:15:41 -0500

    created project with create creact app and added files for homepage

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './components/homepage/HomePage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
